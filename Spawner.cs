230d1c997e218e0498c552f42e480c8458d9eb7f
Joona <joona.virmajoki@gmail.com>
2024-03-22 10:38:42 +0200

    Added documentation to spawner

--- Spawner.cs
+++ Spawner.cs
@@ -1,24 +1,29 @@
-using MyBox;
 using System.Collections;
 using UnityEngine;
 
+// Spawner class for spawning prefabs at a user-defined frequency.
 public class Spawner : MonoBehaviour {
-    // Simple spawner script
-    // Instantiates copies of user defined prefab at parent transform position
-    
-    public GameObject prefabToSpawn; // The prefab to spawn
-    public float spawnFrequency = 1f; // The frequency of spawning
+    // Spawning is done by instantiating a prefab at the parent object's position at a defined frequency.
+    // Should not be used for spawning a large number of objects at once, as it may cause performance issues.
+    // Implement pooling for spawning a large number of objects.
 
-    private Coroutine spawnCoroutine; // Reference to the spawn coroutine
+    // The prefab to spawn.
+    public GameObject prefabToSpawn;
 
-    [ButtonMethod]
+    // The frequency of spawning.
+    public float spawnFrequency = 1f;
+
+    // Reference to the spawn coroutine.
+    private Coroutine spawnCoroutine;
+
+    // Starts the spawning process.
     public void StartSpawning() {
         if (spawnCoroutine == null) {
             spawnCoroutine = StartCoroutine(SpawnAtFrequency());
         }
     }
 
-    [ButtonMethod]
+    // Stops the spawning process.
     public void StopSpawning() {
         if (spawnCoroutine != null) {
             StopCoroutine(spawnCoroutine);
@@ -26,6 +31,7 @@ public class Spawner : MonoBehaviour {
         }
     }
 
+    // Coroutine that spawns the prefab at the parent object's position at the defined frequency.
     private IEnumerator SpawnAtFrequency() {
         while (true) {
             // Instantiate the prefab at the parent object's position
